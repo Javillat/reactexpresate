9ee229a6b49f2a5b436cf884b9e895f99bc4942e
Javier Villatoro <wemakesoftx@gmail.com>
2023-05-21 22:54:52 -0600

    feat: Crear detalle de pelicula

---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+
+import { getDetails, cleanData } from "../../redux/actions/actions";
+
+export default function Detail() {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { id } = useParams();
+    const detail = useSelector((dataDetail) => dataDetail.detail);
+
+    useEffect(() =>{
+        dispatch(getDetails(id));
+        return() => {
+            dispatch(cleanData());
+        };
+    }, []);
+
+    const backHandler = (event) => {
+        history.push("/home");
+    };
+
+    return (
+        (detail !== undefined) ? 
+        (
+            <div className="containerDetail">
+                <span>
+                    <button onClick={backHandler}>Ir atras</button>
+                </span>
+                <p>Detalle de pelicula...</p>
+            </div>
+        ) : ("Loading...")
+    );
+};
