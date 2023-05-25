c4edf4be1dc846115d909a75fab657b45c056076
Javier Villatoro <wemakesoftx@gmail.com>
2023-05-24 22:02:17 -0600

    chore Agregar action detalle de pelicula

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { URL, apikey } from "../../config/config";
 
 export const GET_MOVIES = "GET_MOVIES";
+export const GET_DETAIL = "GET_DETAIL";
 
 export function getMovies(nombre){
     return async (disppatch) => {
@@ -13,5 +14,22 @@ export function getMovies(nombre){
         })
     }
 }
+
+export function getDetails(id) {
+    return async (dispatch) => {
+        const detail = await axios.get(`${URL}/?i=${id}&apikey=${apikey}`);
+        console.log(detail);
+        dispatch({
+            type: "GET_DETAIL",
+            payload: detail.data.Search,
+        });
+    };
+};
+
+export function cleanData(){
+    return {
+        type: "CLEAN_DATA",
+    };
+};
 
 //https://www.omdbapi.com/?s=rocky&page=1&apikey=4825d224
